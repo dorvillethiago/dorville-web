84f0fe6a9ca8ecad186c9927d41fe49bc211ba5c
dorvillethiago <dorville.dev@gmail.com>
2024-05-10 16:07:59 -0300

    update 10/05/2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '@/app/hero'
+import Advantages from '@/app/advantages'
 
 export default function Page() {
-    return (
-        <>
-            <Hero />
-        </>
-    )
+	return (
+		<>
+			<Hero />
+			<Advantages />
+		</>
+	)
 }
