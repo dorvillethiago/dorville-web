6fa77c29b59674c15207bc104759cc7135702f89
dorvillethiago <dorville.dev@gmail.com>
2024-06-04 21:59:49 -0300

    chore: add zustand and shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,36 +1,58 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-    content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        extend: {
-            colors: {
-                text: 'var(--text)',
-                background: 'var(--background)',
-                primary: 'var(--primary)',
-                secondary: 'var(--secondary)',
-                'light-primary': 'var(--light-primary)',
-                accent: 'var(--accent)',
-            },
-            keyframes: {
-                animatedgradient: {
-                    '0%': { backgroundPosition: '0% 50%' },
-                    '50%': { backgroundPosition: '100% 50%' },
-                    '100%': { backgroundPosition: '0% 50%' },
-                },
-            },
-            backgroundSize: {
-                '300%': '300%',
-            },
-            animation: {
-                gradient: 'animatedgradient 6s ease infinite alternate',
-            },
-        },
-    },
-    plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				text: 'var(--text)',
+				background: 'var(--background)',
+				primary: 'var(--primary)',
+				secondary: 'var(--secondary)',
+				'light-primary': 'var(--light-primary)',
+				accent: 'var(--accent)',
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				foreground: 'hsl(var(--foreground))',
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				}
+			},
+			keyframes: {
+				animatedgradient: {
+					'0%': { backgroundPosition: '0% 50%' },
+					'50%': { backgroundPosition: '100% 50%' },
+					'100%': { backgroundPosition: '0% 50%' }
+				}
+			},
+			backgroundSize: {
+				'300%': '300%'
+			},
+			animation: {
+				gradient: 'animatedgradient 6s ease infinite alternate',
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out'
+			}
+		}
+	},
+	plugins: [require('tailwindcss-animate')]
 }
 export default config
